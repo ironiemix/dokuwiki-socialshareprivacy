f29909a87814dd34c04995804c73ab33a3f6e83c
Mark <mc.prins@gmail.com>
2012-08-21 13:35:52 +0200

    adding dutch translation

---
+++ lang/nl/settings.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Dutch language file for socialshareprivacy plugin
+ *
+ * @author Frank Schiebel <frank@linuxmuster.net>
+ * @author Mark C. Prins <mprins@users.sf.net>
+ */
+
+// Global settings for socialshareprivacy plugin
+$lang['global_info_link']    = "Link naar gedetailleerde gegevens beschermings informatie, dafeult is het heise-Artikel.";
+$lang['global_txt_help']     = "Mouseover tekst van de I-pictogrammen";
+$lang['global_settings_perma']    = "Kop van het menu Instellingen";
+$lang['global_cookie_expires']    = "De geldigheidsduur van de cookie in dagen";
+// Facebook settings for socialshareprivacy plugin
+$lang['facebook_status']    = "Der User hat Facebook zur Auswahl/nicht zur Auswahl";
+$lang['facebook_txt_info']    = "Mouseover tekst van de Facebook Delen knop";
+$lang['facebook_perma_option']    = "Der User hat die Option, sich Facebook mittels Cookie dauerhaft ohne vorherige Aktivierung einblenden zu lassen.";
+$lang['facebook_display_name']    = "Schrijfwijze van de Facebook-dienst in de instellingen";
+$lang['facebook_language']    = "Facebook taal instelling";
+$lang['facebook_action']    = "Beschriftung des Facebook-Buttons: Empfehlen (recommend) oder Gefällt mir (like)";
+// Twitter settings for socialshareprivacy plugin
+$lang['twitter_status']    = "De gebruiker heeft de keuze uit Twitter zur Auswahl/nicht zur Auswahl";
+$lang['twitter_txt_info']    = "Mouseover tekst van de Twitter-knop";
+$lang['twitter_perma_option']    = "Der User hat die Option, sich Twitter mittels Cookie dauerhaft ohne vorherige Aktivierung einblenden zu lassen.";
+$lang['twitter_display_name']    = "Schreibweise des Twitter-Service in den Optionen";
+$lang['twitter_language']    = "Twitter taal instelling (Default: 'en' ja, Tweet klinkt beter als getwitter)";
+// Google-Plus settings for socialshareprivacy plugin
+$lang['gplus_status']    = "Der User hat Google+ zur Auswahl/nicht zur Auswahl";
+$lang['gplus_txt_info']    = "Mouseover tekst van de Google Plus Share Knop";
+$lang['gplus_perma_option']    = "Der User hat die Option, sich Google+ mittels Cookie dauerhaft ohne vorherige Aktivierung einblenden zu lassen.";
+$lang['gplus_display_name']    = "Schreibweise des GooglePlus-Service in den Optionen";
+$lang['gplus_language']    = "GooglePlus taal instelling";
+
+
+
+//Setup VIM: ex: et ts=4 :
